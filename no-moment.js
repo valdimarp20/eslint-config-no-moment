82f3fcaed46dabc6ed0bd19c344e0915df4225b2
Valdimar John Parks <valdimarp20@ru.is>
2023-04-05 19:59:38 +0000

    feat: :sparkles: rule to forbid usage of Moment.js library

---
+++ no-moment.js
@@ -0,0 +1,33 @@
+module.exports = {
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Forbid the use of the Moment.js library",
+      category: "Possible Errors",
+    },
+    messages: {
+      noMoment: "The use of Moment.js library is forbidden.",
+    },
+  },
+  create: function (context) {
+    return {
+      ImportDeclaration(node) {
+        if (node.source.value === "moment") {
+          context.report({
+            node,
+            messageId: "noMoment",
+          });
+        }
+      },
+      CallExpression(node) {
+        const { callee } = node;
+        if (callee.type === "Identifier" && callee.name === "moment") {
+          context.report({
+            node,
+            messageId: "noMoment",
+          });
+        }
+      },
+    };
+  },
+};
