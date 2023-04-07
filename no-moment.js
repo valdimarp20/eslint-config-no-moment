218e1d80d00fea624936e10710833569278d3e4c
Valdimar John Parks <valdimarp20@ru.is>
2023-04-07 17:06:02 +0000

    chore: :ambulance: fixed package name -> eslint-plugin-no-moment

--- no-moment.js
+++ no-moment.js
@@ -1,35 +1,29 @@
 module.exports = {
   meta: {
-    type: "error",
-    docs: {
-      description: "forbid usage of moment",
-      category: "Possible Errors",
-    },
+    type: "problem",
     messages: {
       noMoment: "Usage of Moment.js library is forbidden.",
     },
   },
-  create: function (context) {
-    return {
-      ImportDeclaration(node) {
-        if (node.source.value === "moment") {
-          context.report({
-            node: node,
-            messageId: "noMoment",
-          });
-        }
-      },
-      CallExpression(node) {
-        if (
-          node.callee.name === "require" &&
-          node.arguments[0].value === "moment"
-        ) {
-          context.report({
-            node: node,
-            messageId: "noMoment",
-          });
-        }
-      },
-    };
-  },
+  create: (context) => ({
+    ImportDeclaration: (node) => {
+      if (node.source.value === "moment") {
+        context.report({
+          node: node,
+          messageId: "noMoment",
+        });
+      }
+    },
+    CallExpression: (node) => {
+      if (
+        node.callee.name === "require" &&
+        node.arguments[0].value === "moment"
+      ) {
+        context.report({
+          node: node,
+          messageId: "noMoment",
+        });
+      }
+    },
+  }),
 };
